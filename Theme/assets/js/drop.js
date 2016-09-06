7e7e00bf7c2da72ff865856a14ddc1afbf4f12c0
jordanomac@gmail.com <jordanomac@gmail.com>
2016-09-05 19:30:41 -0600

    language change

---
+++ Theme/assets/js/drop.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Jordan on 9/4/16.
+ */
+var dropInstance = new Drop({
+    target: document.querySelector('resume-button'),
+    content: 'Welcome to the future',
+    classes: 'drop-theme-arrows',
+    position: 'bottom left',
+    openOn: 'hover'
+})
