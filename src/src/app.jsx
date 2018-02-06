ff2ae732a213a809e3cd2bfa38e63a48dba72c18
jordan.purinton <jordanomac@gmail.com>
2018-02-05 23:38:13 -0700

    Add a better fade plugin.

--- src/src/app.jsx
+++ src/src/app.jsx
@@ -1,27 +1,23 @@
 import * as React from 'react';
-import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 import Header from './header';
 import About from './about';
 import Portfolio from './portfolio';
 import Skills from './skills';
 import Footer from './footer';
 
-const Container = styled.div`
-    vertical-align: middle;
-    align-items: center;
-    margin: 0;
-    padding: 0;`;
-
 class App extends React.Component {
     render() {
         return (
-            <Container>
-                <Header/>
-                <About/>
-                <Portfolio/>
-                <Skills/>
-                <Footer/>
-            </Container>
+            <Fade>
+                <div>
+                    <Header/>
+                    <About/>
+                    <Portfolio/>
+                    <Skills/>
+                    <Footer/>
+                </div>
+            </Fade>
         );
     }
 }
