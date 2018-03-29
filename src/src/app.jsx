580749440e6b8bd80d7832cb9f4d2d44ff9b2351
Jordan Purinton <jordanomac@gmail.com>
2018-03-29 12:03:22 -0600

    Add fade animation to whole page to make loading fonts less jarring.

--- src/src/app.jsx
+++ src/src/app.jsx
@@ -4,16 +4,19 @@ import About from './about';
 import Portfolio from './portfolio';
 import Skills from './skills';
 import Footer from './footer';
+import Fade from 'react-reveal/Fade';
 
 class App extends React.Component {
     render() {
         return (
             <section>
+                <Fade>
                 <Header/>
                 <About/>
                 <Portfolio/>
                 <Skills/>
                 <Footer/>
+                </Fade>
             </section>
         );
     }
