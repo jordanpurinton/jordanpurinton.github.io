b22a183a25bcca8b7b9fa227dad3f251ef5db7c9
Jordan Purinton <jordanomac@gmail.com>
2018-05-30 14:07:53 -0600

    Add logrocket config remove unecessary typing stuff

--- src/src/index.jsx
+++ src/src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app';
 import registerServiceWorker from './registerServiceWorker';
+import LogRocket from 'logrocket';
 
-ReactDOM.render(<App/>, document.getElementById('root') as HTMLElement);
+LogRocket.init('ksqh7f/sayathing');
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
