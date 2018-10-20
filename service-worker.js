e38f723cd4c5dc9892e051d90ed98858117f99a4
jordanpurinton <jordanomac@gmail.com>
2018-10-19 21:33:31 -0600

    Do the painstaking work of increasing the icon size

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.27b9e68101cc7abd21c8e0f8c93beb2a.js"
+  "./precache-manifest.ad1fbf0815e0b1dcd8417c037a816185.js"
 );
 
 workbox.clientsClaim();
