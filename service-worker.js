7fbf71336e77ce178c03c2685256a4e34bffd97c
Jordan Purinton <jordanomac@gmail.com>
2019-10-06 00:40:19 -0400

    {Cleanup:-New release}

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8c6f5037a81a31aacf71ce9742beb797.js"
+  "./precache-manifest.6cd055666253088dbed71c5fba80fd8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
