ea7e7ecc632e54b42ad447311bcb9c4a91fb80a0
jordanpurinton <jordanomac@gmail.com>
2020-07-25 21:13:10 -0400

    New release

---
+++ precache-manifest.c79a42b5de6cd9c29844fa2e73d8e12b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ee3e7bdb1f88affd3ae8516faff054",
+    "url": "./index.html"
+  },
+  {
+    "revision": "00afe7709f8ca6d16469",
+    "url": "./static/css/main.c72f464a.chunk.css"
+  },
+  {
+    "revision": "4d7fd0cb0e61cfb6bb46",
+    "url": "./static/js/2.f60edbfc.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "./static/js/2.f60edbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00afe7709f8ca6d16469",
+    "url": "./static/js/main.03bc84b9.chunk.js"
+  },
+  {
+    "revision": "c241769e81e1d81aa1c1",
+    "url": "./static/js/runtime-main.bf00a06f.js"
+  },
+  {
+    "revision": "ba307478f28385b27ab4280197d6af04",
+    "url": "./static/media/JordanPurintonResume.ba307478.pdf"
+  },
+  {
+    "revision": "c1572f2662ee4f6630907b97a7f991fe",
+    "url": "./static/media/dataviz.c1572f26.png"
+  },
+  {
+    "revision": "5155db70d4fa66b3a8c4c1e9367f6c18",
+    "url": "./static/media/espn-logo.5155db70.jpg"
+  },
+  {
+    "revision": "7bf3899f08703e69521253c346f5ca29",
+    "url": "./static/media/espn.7bf3899f.png"
+  },
+  {
+    "revision": "0f68f0d0b5c91c7320e121172b1d4f11",
+    "url": "./static/media/gatherboard-app.0f68f0d0.png"
+  },
+  {
+    "revision": "277bc835f70c48b70a86dc749008f2a1",
+    "url": "./static/media/geofli.277bc835.png"
+  },
+  {
+    "revision": "bb438c27ec0d04d2f152e55838bf586d",
+    "url": "./static/media/jordan-purinton-pic.bb438c27.png"
+  },
+  {
+    "revision": "5b6478f933b5f0d8234c0ed242c79402",
+    "url": "./static/media/kaimin-logo.5b6478f9.jpg"
+  },
+  {
+    "revision": "fa1b309c5b8b4254cc7b1d7d2008559e",
+    "url": "./static/media/kaimin.fa1b309c.png"
+  },
+  {
+    "revision": "d9b67e95cfffdae05763d8d0ad31f3f6",
+    "url": "./static/media/os-logo.d9b67e95.jpg"
+  },
+  {
+    "revision": "3a87ae2230529f32204d1e22434717a1",
+    "url": "./static/media/os.3a87ae22.png"
+  },
+  {
+    "revision": "234f46f71f502a48fdc2fc14c2b52fab",
+    "url": "./static/media/umt-logo.234f46f7.jpg"
+  }
+]);
