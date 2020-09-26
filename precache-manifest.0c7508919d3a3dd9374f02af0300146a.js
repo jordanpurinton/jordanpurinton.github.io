976dc12daca675f70486c9f566f6c487342fce99
Jordan Purinton <jordanomac@gmail.com>
2020-09-26 18:54:04 -0400

    asdf

---
+++ precache-manifest.0c7508919d3a3dd9374f02af0300146a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201bd1d778f98a5edbfc0617ebffa014",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ccd3eca434a5bbf260c",
+    "url": "./static/css/main.9aaeb497.chunk.css"
+  },
+  {
+    "revision": "c869ae9d46191449e998",
+    "url": "./static/js/2.35773909.chunk.js"
+  },
+  {
+    "revision": "0637145306ecc4a6084a96b2a92015d5",
+    "url": "./static/js/2.35773909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ccd3eca434a5bbf260c",
+    "url": "./static/js/main.3a38c8cb.chunk.js"
+  },
+  {
+    "revision": "c241769e81e1d81aa1c1",
+    "url": "./static/js/runtime-main.bf00a06f.js"
+  },
+  {
+    "revision": "c1572f2662ee4f6630907b97a7f991fe",
+    "url": "./static/media/dataviz.c1572f26.png"
+  },
+  {
+    "revision": "5155db70d4fa66b3a8c4c1e9367f6c18",
+    "url": "./static/media/espn-logo.5155db70.jpg"
+  },
+  {
+    "revision": "7bf3899f08703e69521253c346f5ca29",
+    "url": "./static/media/espn.7bf3899f.png"
+  },
+  {
+    "revision": "0f68f0d0b5c91c7320e121172b1d4f11",
+    "url": "./static/media/gatherboard-app.0f68f0d0.png"
+  },
+  {
+    "revision": "277bc835f70c48b70a86dc749008f2a1",
+    "url": "./static/media/geofli.277bc835.png"
+  },
+  {
+    "revision": "bb438c27ec0d04d2f152e55838bf586d",
+    "url": "./static/media/jordan-purinton-pic.bb438c27.png"
+  },
+  {
+    "revision": "5b6478f933b5f0d8234c0ed242c79402",
+    "url": "./static/media/kaimin-logo.5b6478f9.jpg"
+  },
+  {
+    "revision": "fa1b309c5b8b4254cc7b1d7d2008559e",
+    "url": "./static/media/kaimin.fa1b309c.png"
+  },
+  {
+    "revision": "d9b67e95cfffdae05763d8d0ad31f3f6",
+    "url": "./static/media/os-logo.d9b67e95.jpg"
+  },
+  {
+    "revision": "3a87ae2230529f32204d1e22434717a1",
+    "url": "./static/media/os.3a87ae22.png"
+  },
+  {
+    "revision": "234f46f71f502a48fdc2fc14c2b52fab",
+    "url": "./static/media/umt-logo.234f46f7.jpg"
+  }
+]);
